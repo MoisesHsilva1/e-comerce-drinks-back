395950f9ebfd9860e9d2257dc76645dd49e4df83
moisas <moihgm67@gmail.com>
2025-05-14 18:46:04 -0300

    feat: update server

--- app.js
+++ app.js
@@ -1,59 +1,45 @@
-  import { ConnectToDB } from "./src/config/database/database-connect.js";
-  import express from "express";
-  import dotenv from "dotenv";
-  import https from "https";
-  import fs from "fs";
-  import path from "path";
-  import { fileURLToPath } from "url";
-  import cors from "cors";
-  import morgan from "morgan";
-  import admin from "firebase-admin";
-  import drinkRoute from "./src/routes/drink.route.js";
-  import userRoute from "./src/routes/user.route.js";
+import { ConnectToDB } from "./src/config/database/database-connect.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import admin from "firebase-admin";
+import drinkRoute from "./src/routes/drink.route.js";
+import userRoute from "./src/routes/user.route.js";
 
-  dotenv.config();
+dotenv.config();
 
-  const app = express();
-  const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      type: process.env.FIREBASE_TYPE,
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      client_id: process.env.FIREBASE_CLIENT_ID,
-      auth_uri: process.env.FIREBASE_AUTH_URI,
-      token_uri: process.env.FIREBASE_TOKEN_URI,
-      auth_provider_x509_cert_url:
-        process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-      universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-    }),
-  });
+admin.initializeApp({
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  }),
+});
 
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
 
-  app.use(cors());
-  app.use(express.json());
-  app.use(morgan("dev"));
 
-  const db = new ConnectToDB();
-  db.connect();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
-  app.use("/drink", drinkRoute);
-  app.use("/user", userRoute);  
+const db = new ConnectToDB();
+db.connect();
 
-  const options = {
-    key: fs.readFileSync(path.join(__dirname, "localhost-key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "localhost-cert.pem")),
-  };
+app.use("/drink", drinkRoute);
+app.use("/user", userRoute);
 
-  const server = https.createServer(options, app);
 
-  app.listen(port, () => {
-    console.log(`App listening on https://localhost:${port}`);
-  });
-  
+export default app;
